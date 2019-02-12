fa40aad6673ade613924c4cd98f1cd650ae0d111
Inc <35619461+vials@users.noreply.github.com>
2019-02-12 17:09:18 -0600

    Create UserID.py

---
+++ UserID.py
@@ -0,0 +1,12 @@
+import requests, re, json
+from bs4 import BeautifulSoup
+
+TUsername = raw_input('Enter User To Resolve: ')
+
+r = requests.get('https://www.instagram.com/'+TUsername+'/')
+soup = BeautifulSoup(r.content, "lxml")
+scripts = soup.find_all('script', type="text/javascript", text=re.compile('window._sharedData'))
+stringified_json = scripts[0].get_text().replace('window._sharedData = ', '')[:-1]
+z = json.loads(stringified_json)['entry_data']['ProfilePage'][0]
+y = z["logging_page_id"].replace("profilePage_", "")
+print('Resolved User ID: '+y)
