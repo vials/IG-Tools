2f8fc7f3113352139ce2ffd6d4e1856074b4f0ec
Inc <35619461+vials@users.noreply.github.com>
2019-04-15 19:16:38 -0500

    Create gay-auth.py

---
+++ gay-auth.py
@@ -0,0 +1,25 @@
+import requests
+from colorama import Fore, init
+init()
+
+gayUsers = []
+
+with open('gayUsers.txt', 'r') as f:
+    for line in f:
+        for user in line.split():
+            gayUsers.append(user)
+
+def gayAuth():
+    print('Welcome To Gay Auth')
+    print('-'*20)
+
+    r = requests.get('https://www.pastebin.com/raw/nv58aVHJ')
+    for i in r.content.decode('utf-8').split():
+        if i in gayUsers:
+            print('['+Fore.GREEN+'+'+Fore.WHITE+']'+' Authenticated Gay User')
+        else:
+            print('['+Fore.RED+'-'+Fore.WHITE+']'+' Failed To Authenticate Gay User')
+
+if __name__ == '__main__':
+    gayAuth()
+#I had to do it if all the other C+P Auth turbo people were doing it
